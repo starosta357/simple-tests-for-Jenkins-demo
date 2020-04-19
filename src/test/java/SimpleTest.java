1ef96448b39c1abd075a011272196e0030b4ca11
Mykyta Tkachenko <nikitargets@gmail.com>
2020-04-19 20:46:15 +0300

    add random test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,4 +1,7 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Random;
 
 public class SimpleTest {
 
@@ -10,6 +13,17 @@ public class SimpleTest {
     @Test
     public void redTest() {
         throw new NullPointerException();
+    }
+
+    @Test
+    public void randomTest() {
+        Random random = new Random();
+
+        // Generate random integers in range 0 to 9
+        int randomNumber = random.nextInt(10);
+
+        System.out.println(String.format("The number is %d", randomNumber));
+        Assert.assertTrue(5 >= randomNumber);
     }
 
 }
